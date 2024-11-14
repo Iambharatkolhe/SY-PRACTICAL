6393cd9b6d68021356cccf4f2c070cba54807dcc
Bharat kolhe <163564571+Iambharatkolhe@users.noreply.github.com>
2024-11-14 08:42:33 +0530

    Add files via upload

---
+++ DSPS-PRACTICAL/05-Infix To Postfix/infixtopostfix.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int precedence(char op) {
+    if (op == '+' || op == '-') {
+        return 1;
+    } else if (op == '*' || op == '/') {
+        return 2;
+    } else if (op == '^') {
+        return 3;
+    }
+    return 0;
+}
+
+bool isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+}
+
+string infixToPostfix(string infix) {
+    stack<char> s;
+    string postfix = "";
+
+    for (int i = 0; i < infix.length(); i++) {
+        char c = infix[i];
+
+        if (isalnum(c)) {
+            postfix += c;
+        } else if (c == '(') {
+            s.push(c);
+        } else if (c == ')') {
+            while (!s.empty() && s.top() != '(') {
+                postfix += s.top();
+                s.pop();
+            }
+            s.pop();
+        } else if (isOperator(c)) {
+            while (!s.empty() && precedence(s.top()) >= precedence(c)) {
+                postfix = s.top();
+                s.pop();
+            }
+            s.push(c);
+        }
+    }
+
+    while (!s.empty()) {
+        postfix += s.top();
+        s.pop();
+    }
+
+    return postfix;
+}
+
+int main() {
+    string infix_expression;
+
+    cout << "Enter an infix expression: ";
+    getline(cin, infix_expression);
+
+    string postfix_expression = infixToPostfix(infix_expression);
+
+    cout << "Postfix expression: " << postfix_expression << endl;
+
+    return 0;
+}
